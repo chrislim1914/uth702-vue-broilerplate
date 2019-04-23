442b133227f0b249d9f13ce98a72350cae156d6d
Daniel Sarabusing <daniels.uth702@gmail.com>
2019-04-23 10:16:47 +0800

    updated app vue scss and main component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -16,13 +16,6 @@ export default {
 }
 </script>
 
-<style lang='scss'>
-  * {
-    margin: 0;
-    padding: 0;
-  }
-
-  .debugger {
-    border: 2px solid red;
-  }
+<style lang='scss' scoped>
+  @import '~scss/main.scss';
 </style>
