91b0de13ba9fe174660d29d8f9ccba0f59e615d5
Daniel Sarabusing <daniels.uth702@gmail.com>
2019-04-23 10:47:12 +0800

    updated template

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,32 @@
 <template>
   <!-- app-main -->
   <div id="app-main">
-    <h1>Hello, app</h1>
+    <div class="container mx-auto">
+      <div class="row text-center mt-5 pt-5">
+        <div class="col-6 mx-auto">
+          <h1 class="font-weight-light">New project!</h1>
+          <p class="lead text-justify mt-3">
+            &emsp; Welcome to your new project, this is a pre-made template that usually
+            the front-end web team needs...
+          </p>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col-6 mx-auto">
+          <h1 class="font-weight-light">Git repository</h1>
+          <p class="mt-3">
+            <code>
+              origin  git@gitlab.com:uth702/uth702_vue_boilerplate.git (fetch)
+            </code>
+          </p>
+          <p>
+            <code>
+              origin  git@gitlab.com:uth702/uth702_vue_boilerplate.git (push)
+            </code>
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
